3d045a29dc5c21677ea4f8f9fc421d49e777233f
Marcin Czepiela <czepimm200@gmail.com>
2020-06-14 19:05:54 +0200

    Build 1.0

---
+++ precache-manifest.4b6927b0c62ceca0fb4b0aea689fb454.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f194d39991858e2fabd69bbfa9c15f4",
+    "url": "//czepim200.github.io/PIW-LAB05/index.html"
+  },
+  {
+    "revision": "22064ada071b90b73eb8",
+    "url": "//czepim200.github.io/PIW-LAB05/static/css/main.8353ba4e.chunk.css"
+  },
+  {
+    "revision": "f26185b346ecfb9b4958",
+    "url": "//czepim200.github.io/PIW-LAB05/static/js/2.1e7bcaff.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "//czepim200.github.io/PIW-LAB05/static/js/2.1e7bcaff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22064ada071b90b73eb8",
+    "url": "//czepim200.github.io/PIW-LAB05/static/js/main.ec404492.chunk.js"
+  },
+  {
+    "revision": "c93b394f59bc7560ba2e",
+    "url": "//czepim200.github.io/PIW-LAB05/static/js/runtime-main.957ae05b.js"
+  },
+  {
+    "revision": "108adade55bb44ed1d450c3623c3e2a2",
+    "url": "//czepim200.github.io/PIW-LAB05/static/media/cat.108adade.jpg"
+  },
+  {
+    "revision": "f66606e0e2d6f46928b9960c199a6480",
+    "url": "//czepim200.github.io/PIW-LAB05/static/media/mockingjay.f66606e0.jpg"
+  },
+  {
+    "revision": "254e2aade1a20e80766f37f209f25845",
+    "url": "//czepim200.github.io/PIW-LAB05/static/media/react.254e2aad.png"
+  }
+]);
